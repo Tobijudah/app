c1be21b397f481b50d6263e7e8c53d6c1227fe4c
Omotosho Oluwatobiju <tobijudah@gmail.com>
2020-08-26 15:05:10 +0100

    app

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ submit.addEventListener('click', e => {
   const password = document.querySelector('#password').value
   const name = document.querySelector('#name').value
 
+  if (email == '' || name == '' || password == '') {
+    alert('fill out all fields')
+    return
+  }
+
   localStorage.setItem('email', email)
   localStorage.setItem('password', password)
   localStorage.setItem('name', name)
